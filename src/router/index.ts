c25f5c9dae3b3eb50e6a283564cf5c22dbc51e82
John Munger <johntmunger@gmail.com>
2021-12-03 12:05:47 -0700

    fix(router): add routes and build the products template

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,20 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Admin from '../pages/admin/Admin.vue';
+import Products from '../pages/admin/Products.vue';
+import Main from '../pages/Main.vue';
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'admin',
-    component: Admin
-  },
+    {
+        path: '/',
+        component: Main
+    },
+    {
+        path: '/admin',
+        component: Admin,
+        children: [
+            { path: 'products', component: Products }
+        ]
+    },
 ]
 
 const router = createRouter({
