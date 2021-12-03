55dd7c266935418aa542dcfc66c8ef4f5f27234e
John Munger <johntmunger@gmail.com>
2021-12-03 12:52:27 -0700

    fix(setup): get the products data into the component

--- src/pages/admin/Products.vue
+++ src/pages/admin/Products.vue
@@ -3,20 +3,20 @@
     <table class="table table-striped table-sm">
         <thead>
             <tr>
-            <th scope="col">#</th>
-            <th scope="col">Header</th>
-            <th scope="col">Header</th>
-            <th scope="col">Header</th>
-            <th scope="col">Header</th>
+                <th scope="col">#</th> 
+                <th scope="col">Image</th>
+                <th scope="col">Category</th>
+                <th scope="col">Title</th>
+                <th scope="col">Action</th>  
             </tr>
         </thead>
         <tbody>
-            <tr>
-            <td>1,001</td>
-            <td>random</td>
-            <td>data</td>
-            <td>placeholder</td>
-            <td>text</td>
+            <tr v-for="product in products" :key="product.id">
+                <td>{{ product.id }}</td>
+                <td><img :src="product.image" height="50" /></td>
+                <td>{{ product.category }}</td>
+                <td>{{ product.title }}</td>
+                <td>Delete Button Here</td> <!-- this is where you left off -->
             </tr>
         </tbody>
     </table>
@@ -24,7 +24,21 @@
 </template>
 
 <script>
+import { ref, onMounted } from 'vue';
 export default {
-    name: "Products"
+    name: "Products",
+    setup() {
+        const products = ref([]);
+
+        onMounted(async () => {
+            const response = await fetch('https://fakestoreapi.com/products');
+
+            products.value = await response.json();
+        });
+
+        return {
+            products
+        }
+    }
 }
 </script>
