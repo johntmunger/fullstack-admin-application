79ad0b1e6f92badd125e8465e4fdc6fe98273ad7
John Munger <johntmunger@gmail.com>
2021-12-03 11:43:48 -0700

    fix(template): get the components and html css in

---
+++ src/pages/admin/Admin.vue
@@ -0,0 +1,46 @@
+<template>
+<div>
+    <Nav />
+    <div class="container-fluid">
+        <div class="row">
+            <Menu />
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                <div class="table-responsive">
+                    <table class="table table-striped table-sm">
+                    <thead>
+                        <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Header</th>
+                        <th scope="col">Header</th>
+                        <th scope="col">Header</th>
+                        <th scope="col">Header</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <td>1,001</td>
+                        <td>random</td>
+                        <td>data</td>
+                        <td>placeholder</td>
+                        <td>text</td>
+                        </tr>
+                    </tbody>
+                    </table>
+                </div>
+            </main>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+import Nav from '@/components/Nav';
+import Menu from '@/components/Menu';
+export default {
+    name: "Admin",
+    components: { 
+        Nav,
+        Menu
+    }
+}
+</script>
